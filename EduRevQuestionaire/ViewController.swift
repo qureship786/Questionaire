0938d664ef3e91dc7a11bee91d195f881fff526d
Parvez Qureshi <parvezq@rediffmail.com>
2020-11-13 08:53:50 +0530

    Initial Commit

---
+++ EduRevQuestionaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EduRevQuestionaire
+//
+//  Created by Parvez Qureshi on 13/11/20.
+//  Copyright © 2020 Parvez Qureshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
